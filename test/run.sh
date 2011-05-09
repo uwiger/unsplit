10fd753c9f28782fdca9c4d785f59d5e7c17c61c
Ahmed Omar <spawn.think@gmail.com>
2011-05-09 17:23:36 +0200

    Refactored the test. Now nodes are configurable, in split.cfg, also timeouts. The test now will start two ct_slave nodes and run mnesia and unsplit there. The drawback for that, we don't get code coverage. One problem needs to be solved, how to know that unsplit is done with fixing the table.

--- test/run.sh
+++ test/run.sh
@@ -1,6 +1,6 @@
 !# /bin/sh
 cd ../src; make; cd -;
-run_test -pa ../ebin -include ../src/ -dir $PWD -cover config/cover.conf -logdir $PWD/log;
+run_test -pa ../ebin -include ../src/ -config config/split.cfg -dir $PWD -repeat ${1-1} -cover config/cover.conf -logdir $PWD/log;
 rm *.beam;
 cd ../src; make clean
 
